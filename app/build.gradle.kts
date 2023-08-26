eeed342bde052ca36d10803a96301e2a29401b7a
Viatcheslav Ehrmann <viatcheslav.ehrmann@gmail.com>
2023-08-26 17:36:26 +0200

    #release 1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.graytsar.livewallpaper"
         minSdk = 23
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.1.3"
+        versionCode = 16
+        versionName = "1.1.4"
 
         multiDexEnabled = true
 
